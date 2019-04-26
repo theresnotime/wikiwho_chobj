77fe26063e1e4640f7d6d96a12891a73c65a7796
Ulloa, Roberto <roberto.ur@gmail.com>
2019-04-26 10:28:32 +0200

    create change object not needed anymore

--- cobject.py
+++ cobject.py
@@ -15,45 +15,6 @@ class ChangeObject:
         self.article_name = article_name
         self.epsilon_size = epsilon_size
 
-    def create_change_object(self, article_name, content_dir="../data/content/",
-                             change_object_dir="../data/change objects/", epsilon_size=30, save=False):
-
-        content_filepath = os.path.join(content_dir, article_name + ".h5")
-        change_object_filepath = os.path.join(
-            change_object_dir, article_name + ".pkl")
-
-        with pd.HDFStore(content_filepath, 'r') as store:
-            # retrieving all rev list and change object from file
-            rev_list = store.get("rev_list")
-            all_rev = store.get("all_tokens")
-            all_tokens = all_rev.to_dict(orient="records")
-
-            # making revision objects
-            revs = rev_list.apply(lambda rev: Revision(
-                rev["id"], rev["timestamp"], rev["editor"]), axis=1)
-            revs.index = rev_list.id
-
-            # Getting first revision object and adding content ot it
-            from_rev_id = revs.index[0]
-            wiki = Wiki(article_name, revs, all_tokens)
-
-            wiki.revisions.iloc[0].content = store["r" + str(from_rev_id)]
-            # adding content to all other revision and finding change object
-            # between them.
-
-            for to_rev_id in list(revs.index[1:]):
-                key = "r" + str(to_rev_id)
-                to_rev_content = store[key]
-                wiki.create_change(from_rev_id, to_rev_id,
-                                   to_rev_content, epsilon_size)
-                from_rev_id = to_rev_id
-
-        if save:
-            with open(change_object_filepath, "wb") as file:
-                pickle.dump(wiki, file)
-
-        return wiki
-
     def df_rev_content(self, key, first=False):
         rev_content = self.ww.api.specific_rev_content_by_rev_id(
             key, o_rev_id=False, editor=False, _in=False, out=False)["revisions"][0][str(key)]["tokens"]
