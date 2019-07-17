7812093fde57085a4f3840b5b96287ccfcb69cd6
Ulloa, Roberto <roberto.ur@gmail.com>
2019-07-17 13:09:45 +0200

    removing the rest of pandas dependencies, comparison of the resulting data is still posible

--- wikiwho_chobj/revision.py
+++ wikiwho_chobj/revision.py
@@ -117,8 +117,331 @@ class Revision:
             neighbour_df.columns = columns
         else:
             neighbour_df = pd.DataFrame(columns=columns)
+
         self.change_df = pd.concat(
             [change, neighbour_df], sort=False, axis=1)
+
+
+        did = 0
+        iid = 0
+
+        chdata = []
+
+        while iid < len(self.ins_left_neigh) and did < len(del_left_neigh):
+
+            if (self.ins_left_neigh[iid] == del_left_neigh[did] and
+                    self.ins_right_neigh[iid] == del_right_neigh[did]):
+
+                isp = to_rev.ins_start_pos[iid]
+                iep = to_rev.ins_end_pos[iid]
+                ln = self.ins_left_neigh[iid]
+                rn = self.ins_right_neigh[iid]
+                dsp = del_start_pos[did]
+                dep = del_end_pos[did]
+
+                ins_slice = slice(isp, iep + 1)
+                del_slice = slice(dsp, dep + 1)
+
+                ln_slice = slice(max(0,ln - epsilon_size), ln + 1)
+                rn_slice = slice(rn, min(rn + epsilon_size + 1, self.tokens.size - 1))
+
+                chdata.append((
+                    # ins_start_pos
+                    isp,
+                    # ins_end_pos
+                    iep,
+                    # left_neigh
+                    ln,
+                    # right_neigh
+                    rn,
+                    # del_start_pos
+                    dsp,
+                    # del_end_pos
+                    dep,
+            
+                    # ins_tokens
+                    to_rev.tokens[ins_slice].tolist(),
+                    # del_tokens
+                    self.tokens[del_slice].tolist(),
+
+                    # ins_tokens_str
+                    tuple(to_rev.values[ins_slice]),
+                    # del_tokens_str
+                    tuple(self.values[del_slice]),
+
+                    # left_token
+                    self.tokens[ln_slice].tolist(),
+                    # right_token
+                    self.tokens[rn_slice].tolist(),
+
+                    # left_token_str
+                    tuple(self.values[ln_slice]),
+                    # right_token_str
+                    tuple(self.values[rn_slice]),
+                ))
+
+                did += 1
+                iid += 1
+            elif (self.ins_left_neigh[iid] <= del_left_neigh[did]):
+
+                isp = to_rev.ins_start_pos[iid]
+                iep = to_rev.ins_end_pos[iid]
+                ln = self.ins_left_neigh[iid]
+                rn = self.ins_right_neigh[iid]
+                dsp = -1
+                dep = -1
+
+                ins_slice = slice(isp, iep + 1)                
+
+                ln_slice = slice(max(0,ln - epsilon_size), ln + 1)
+                rn_slice = slice(rn, min(rn + epsilon_size + 1, self.tokens.size - 1))
+
+                chdata.append((
+                    # ins_start_pos
+                    isp,
+                    # ins_end_pos
+                    iep,
+                    # left_neigh
+                    ln,
+                    # right_neigh
+                    rn,
+                    # del_start_pos
+                    dsp,
+                    # del_end_pos
+                    dep,
+            
+                    # ins_tokens
+                    to_rev.tokens[ins_slice].tolist(),
+                    # del_tokens
+                    [],
+
+                    # ins_tokens_str
+                    tuple(to_rev.values[ins_slice]),
+                    # del_tokens_str
+                    tuple(),
+
+                    # left_token
+                    self.tokens[ln_slice].tolist(),
+                    # right_token
+                    self.tokens[rn_slice].tolist(),
+
+                    # left_token_str
+                    tuple(self.values[ln_slice]),
+                    # right_token_str
+                    tuple(self.values[rn_slice]),
+                ))
+
+                iid += 1
+            elif (self.ins_left_neigh[iid] > del_left_neigh[did]):
+                isp = -1
+                iep = -1
+                ln = del_left_neigh[did]
+                rn = del_right_neigh[did]
+                dsp = del_start_pos[did]
+                dep = del_end_pos[did]
+
+                del_slice = slice(dsp, dep + 1)
+
+                ln_slice = slice(max(0,ln - epsilon_size), ln + 1)
+                rn_slice = slice(rn, min(rn + epsilon_size + 1, self.tokens.size - 1))
+
+                chdata.append((
+                    # ins_start_pos
+                    isp,
+                    # ins_end_pos
+                    iep,
+                    # left_neigh
+                    ln,
+                    # right_neigh
+                    rn,
+                    # del_start_pos
+                    dsp,
+                    # del_end_pos
+                    dep,
+            
+                    # ins_tokens
+                    [],
+                    # del_tokens
+                    self.tokens[del_slice].tolist(),
+
+                    # ins_tokens_str
+                    tuple(),
+                    # del_tokens_str
+                    tuple(self.values[del_slice]),
+
+                    # left_token
+                    self.tokens[ln_slice].tolist(),
+                    # right_token
+                    self.tokens[rn_slice].tolist(),
+
+                    # left_token_str
+                    tuple(self.values[ln_slice]),
+                    # right_token_str
+                    tuple(self.values[rn_slice]),
+                ))
+
+                did += 1
+
+        while iid < len(self.ins_left_neigh):
+
+                isp = to_rev.ins_start_pos[iid]
+                iep = to_rev.ins_end_pos[iid]
+                ln = self.ins_left_neigh[iid]
+                rn = self.ins_right_neigh[iid]
+                dsp = -1
+                dep = -1
+
+                ins_slice = slice(isp, iep + 1)                
+
+                ln_slice = slice(max(0,ln - epsilon_size), ln + 1)
+                rn_slice = slice(rn, min(rn + epsilon_size + 1, self.tokens.size - 1))
+
+                chdata.append((
+                    # ins_start_pos
+                    isp,
+                    # ins_end_pos
+                    iep,
+                    # left_neigh
+                    ln,
+                    # right_neigh
+                    rn,
+                    # del_start_pos
+                    dsp,
+                    # del_end_pos
+                    dep,
+            
+                    # ins_tokens
+                    to_rev.tokens[ins_slice].tolist(),
+                    # del_tokens
+                    [],
+
+                    # ins_tokens_str
+                    tuple(to_rev.values[ins_slice]),
+                    # del_tokens_str
+                    tuple(),
+
+                    # left_token
+                    self.tokens[ln_slice].tolist(),
+                    # right_token
+                    self.tokens[rn_slice].tolist(),
+
+                    # left_token_str
+                    tuple(self.values[ln_slice]),
+                    # right_token_str
+                    tuple(self.values[rn_slice]),
+                ))
+                iid += 1
+
+        while did < len(del_left_neigh):
+                isp = -1
+                iep = -1
+                ln = del_left_neigh[did]
+                rn = del_right_neigh[did]
+                dsp = del_start_pos[did]
+                dep = del_end_pos[did]
+
+                del_slice = slice(dsp, dep + 1)
+
+                ln_slice = slice(max(0,ln - epsilon_size), ln + 1)
+                rn_slice = slice(rn, min(rn + epsilon_size + 1, self.tokens.size - 1))
+
+                chdata.append((
+                    # ins_start_pos
+                    isp,
+                    # ins_end_pos
+                    iep,
+                    # left_neigh
+                    ln,
+                    # right_neigh
+                    rn,
+                    # del_start_pos
+                    dsp,
+                    # del_end_pos
+                    dep,
+            
+                    # ins_tokens
+                    [],
+                    # del_tokens
+                    self.tokens[del_slice].tolist(),
+
+                    # ins_tokens_str
+                    tuple(),
+                    # del_tokens_str
+                    tuple(self.values[del_slice]),
+
+                    # left_token
+                    self.tokens[ln_slice].tolist(),
+                    # right_token
+                    self.tokens[rn_slice].tolist(),
+
+                    # left_token_str
+                    tuple(self.values[ln_slice]),
+                    # right_token_str
+                    tuple(self.values[rn_slice]),
+                ))
+
+                did += 1
+
+        change_df = pd.DataFrame(chdata, columns=[
+            'ins_start_pos', 'ins_end_pos', 
+            'left_neigh', 'right_neigh', 
+            'del_start_pos', 'del_end_pos',
+            "ins_tokens", "del_tokens",
+            "ins_tokens_str", "del_tokens_str",
+            "left_token", "right_token",
+            "left_token_str", "right_token_str"
+        ]).sort_values(['left_neigh', 'right_neigh']).reset_index(drop=True)
+
+        for col in change_df.columns:
+            if not change_df[col].equals(self.change_df[col]):
+                print (col)
+                import ipdb; ipdb.set_trace()  # breakpoint 5c733010 //
+
+
+        
+
+        if not self.change_df.drop(columns=["left_neigh_slice", "right_neigh_slice"]).equals(change_df):
+            import ipdb; ipdb.set_trace()  # breakpoint eda81dfc //
+
+
+
+
+
+# columns = ["ins_tokens", "del_tokens",
+#            "ins_tokens_str", "del_tokens_str",
+#            "left_neigh_slice", "right_neigh_slice",
+#            "left_token", "right_token",
+#            "left_token_str", "right_token_str"]
+
+
+                # yield {
+                #     'page_id': self.p.page_id,
+                #     'from_rev': r1.id,
+                #     'to_rev': r2.id,
+                #     'from_timestamp': r1.timestamp,
+                #     'to_timestamp': r1.timestamp,
+                #     'editor': r2.editor,
+                #     'ins_start_pos': op[0],
+                #     'ins_end_pos': op[1],
+                #     'left_neigh': lstr1[op[0]] if op[0] != op[1] else '' ,
+                #     'right_neigh': lstr1[op[1]-1] if op[0] != op[1] else '',
+                #     'del_start_pos': op[2],
+                #     'del_end_pos': op[3],
+                #     'left_tokens': lids1[max(0, op[0] - 5):op[0]],
+                #     'del_tokens': lids1[op[0]:op[1]],
+                #     'ins_tokens': lids2[op[2]:op[3]],
+                #     'right_tokens': lids1[op[1]:op[1] + 5],
+                #     'left_token_str': lstr1[max(0, op[0] - 5):op[0]],
+                #     'del_tokens_str': lstr1[op[0]:op[1]],
+                #     'ins_tokens_str': lstr2[op[2]:op[3]],
+                #     'right_token_str': lstr1[op[1]:op[1] + 5],
+                #     'text': (lstr1[max(0, op[0] - 5):op[0]],
+                #              lstr1[op[0]:op[1]],
+                #              lstr2[op[2]:op[3]],
+                #              lstr1[op[1]:op[1] + 5])
+                # }
+
+
 
     def find_tokens(self, change, revision, to_rev, epsilon_size):
 
