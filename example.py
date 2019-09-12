f3eb451d60e6ef7e36bb71959a513918d7761231
Roberto Ulloa <roberto.ur@gmail.com>
2019-09-12 18:10:57 +0200

    ChobjerPickle receives opened pickled instead

--- example.py
+++ example.py
@@ -1,13 +1,14 @@
 
-from wikiwho_chobj import Chobjer
+from wikiwho_chobj import Chobjer, ChobjerPickle
 from wikiwho_chobj.utils import Timer
-from wikiwho_chobj_pd import Chobjer as ChobjerPD
+from wikiwho import open_pickle
+
 
 if __name__ == "__main__":
 
     starting_revid = -1
-    ids = [2161298, 1620389, 6187]
-    #ids = [2161298]
+    #ids = [2161298, 1620389, 6187]
+    ids = [2161298]
     #ids = [6886]
     #ids = [2161298]
     
@@ -16,8 +17,16 @@ if __name__ == "__main__":
     #starting_revid = 855387192
 
     for _id in ids:
+
+        ww_pickle = open_pickle(_id, pickle_path='pickles', lang='en')
+        co = ChobjerPickle(ww_pickle=ww_pickle, context=5, starting_revid=starting_revid)
+        with Timer():
+            chobs2 = [x for x in co.iter_chobjs()]
+
         co = Chobjer(article=_id, pickles_path='pickles', lang='en', 
             context=5, starting_revid=starting_revid)
-
         with Timer():
             chobs1 = [x for x in co.iter_chobjs()]
+
+
+
--- wikiwho_chobj/__init__.py
+++ wikiwho_chobj/__init__.py
@@ -8,3 +8,14 @@ except:
 
 from .chobj import Chobjer
 from . import utils
+
+
+
+class ChobjerPickle(Chobjer):
+
+    def __init__(self, ww_pickle, context, starting_revid = -1):
+        self.ww_pickle = ww_pickle       
+        self.article = self.ww_pickle.page_id
+        self.context = context
+        self.revisions = self.ww_pickle.revisions
+        self.starting_revid = starting_revid
